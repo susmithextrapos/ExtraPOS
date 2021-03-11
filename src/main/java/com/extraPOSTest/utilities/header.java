b54a2ca0e202e6359daa603087b2ebd0efccab00
susmithextrapos <susmith4acse@gmail.com>
2021-03-11 15:30:41 +0530

    First Practice

---
+++ src/main/java/com/extraPOSTest/utilities/header.java
@@ -0,0 +1,17 @@
+package com.extraPOSTest.utilities;
+
+public class header {
+
+	public String FormName,Test_Id, Test_Scenario, ExpectResult, FinalResult;
+
+	public header(String formName, String test_Id, String test_Scenario, String expectResult, String finalResult) {
+		FormName = formName;
+		Test_Id = test_Id;
+		Test_Scenario = test_Scenario;
+		ExpectResult = expectResult;
+		FinalResult = finalResult;
+	}
+	
+	
+	
+}
